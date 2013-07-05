283689ad89aa7073f64ad65f2f1634713c9cefb6
Airat Shigapov <airat.shigapov@flatstack.com>
2013-07-05 13:06:56 +0400

    Reduce folders nesting

---
+++ lib/image-picker-rails.rb
@@ -0,0 +1,5 @@
+require "image-picker-rails/version"
+
+module ImagePickerRails
+      # Your code goes here...
+end
--- lib/image/picker/rails.rb
+++
@@ -1,9 +0,0 @@
-require "image/picker/rails/version"
-
-module Image
-  module Picker
-    module Rails
-      # Your code goes here...
-    end
-  end
-end
