a31628ec9b56b1a431b7f9c7bb0b772e96f24412
Airat Shigapov <airat.shigapov@flatstack.com>
2013-07-05 13:02:12 +0400

    Initial commit

---
+++ lib/image/picker/rails.rb
@@ -0,0 +1,9 @@
+require "image/picker/rails/version"
+
+module Image
+  module Picker
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/image/picker/rails/version.rb
@@ -0,0 +1,7 @@
+module Image
+  module Picker
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
